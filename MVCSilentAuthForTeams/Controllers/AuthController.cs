7aed70576798214f8ba2f727cb2e51c83541b190
Nick Wagner <Nick.Wagner@vitalyst.com>
2021-03-18 16:13:04 -0400

    Ported over changes for Silent Authentication

---
+++ MVCSilentAuthForTeams/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace MVCSilentAuthForTeams.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public ActionResult msteamsSilent() { LoadViewBags(); return PartialView(); }
+
+            // GET: Auth
+            public ActionResult msteamsSilentStart()
+            {
+                LoadViewBags();
+                return PartialView();
+            }
+            public ActionResult msteamsSilentEnd()
+            {
+                LoadViewBags();
+                return PartialView();
+            }
+
+            private void LoadViewBags()
+            {
+                ViewBag.ClientId = ConfigurationManager.AppSettings["ida:ClientId"];
+            }
+    }
+}
